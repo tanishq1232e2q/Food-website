1e6be05783ad1fefba8c9554312f493d2cb67593
Tanishq Palkhe <115395213+tanishq1232e2q@users.noreply.github.com>
2024-07-17 19:33:20 +0530

    Update Signup.jsx

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -37,7 +37,7 @@ export default function Signup(props) {
         e.preventDefault()
 
 
-        const response = await fetch("https://food-website-backend-10zz.onrender.com/user/adduser", {
+        const response = await fetch("https://food-website-backend-3ij1.onrender.com/user/adduser", {
             method: "POST",
             headers: {
                 "Content-Type": "Application/json"
