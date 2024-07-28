9dde056b32064f59327d7fd335d4bb887364ca2e
Tanishq Palkhe <115395213+tanishq1232e2q@users.noreply.github.com>
2024-07-28 22:52:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const middleware=require("./middleware/middleware")
 
 
 const corsoption = {
-  origin: "https://food-website-client.onrender.com",
+  origin: "https://food-website-y43f.onrender.com",
   methods: "GET, POST, PUT, DELETE",
   credentials: true
 }
